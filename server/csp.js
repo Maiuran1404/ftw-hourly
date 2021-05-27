3e86cfdabc3f0f6a66fbcb516200163e953f3c00
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2021-05-27 10:33:54 +0200

    intercoom

--- server/csp.js
+++ server/csp.js
@@ -103,8 +103,8 @@ const defaultDirectives = {
     'https://js.intercomcdn.com/vendor-modern.e2013c7e.js',
     'wss://nexus-websocket-a.intercom.io/*',
     'https://api-iam.intercom.io/messenger/web/metrics',
+    'https://js.intercomcdn.com/frame-modern.812aa270.js',
     'https://js.intercomcdn.com/*',
-    'https://js.intercomcdn.com/frame-modern.812aa270.js'
   ],
   styleSrc: [self, unsafeInline, 'fonts.googleapis.com', 'api.mapbox.com'],
 };
