e4357fdc554e0f49720a134b4b55e5565323ddbb
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2021-05-27 17:04:45 +0200

    intercoom

--- server/csp.js
+++ server/csp.js
@@ -44,7 +44,9 @@ const defaultDirectives = {
     'https://js.intercomcdn.com/*',
     'https://js.intercomcdn.com/frame-modern.812aa270.js',
     'https://ws3.hotjar.com',
-    'wss://ws8.hotjar.com'
+    'wss://ws8.hotjar.com',
+    'https://ws15.hotjar.com',
+    '*.hotjar.com'
   ],
   fontSrc: [self, data, 'assets-sharetribecom.sharetribe.com', 'fonts.gstatic.com', 'https://js.intercomcdn.com/fonts/proximanova-semibold.46e3f047.woff'],
   frameSrc: [self, '*.stripe.com', 'https://widget.intercom.io/widget/zyktpfju', 'https://js.intercomcdn.com/frame-modern.812aa270.js'],
