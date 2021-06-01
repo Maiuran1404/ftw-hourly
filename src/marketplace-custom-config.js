014a2b2e7ae21206c6fc4bc250f23c91c8c529de
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2021-06-01 11:54:22 +0200

    with new category setup

--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -39,7 +39,7 @@ export const filters = [
     label: 'Category',
     type: 'SelectSingleFilter',
     group: 'primary',
-    queryParamNames: ['pub_categoy'],
+    queryParamNames: ['pub_category'],
     config: {
       // "key" is the option you see in Flex Console.
       // "label" is set here for the UI only.
