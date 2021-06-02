5d3292738c180c23ff289fca0ee038dd6eb8ba7c
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2021-06-02 12:12:02 +0200

    socials

--- src/config.js
+++ src/config.js
@@ -99,10 +99,10 @@ const canonicalRootURL = process.env.REACT_APP_CANONICAL_ROOT_URL;
 const siteTitle = 'Avita';
 
 // Twitter handle is needed in meta tags (twitter:site). Start it with '@' character
-const siteTwitterHandle = '@sharetribe';
+const siteTwitterHandle = null;
 
 // Instagram page is used in SEO schema (http://schema.org/Organization)
-const siteInstagramPage = null;
+const siteInstagramPage = 'https://www.instagram.com/avitaglobal/';
 
 // Facebook page is used in SEO schema (http://schema.org/Organization)
 const siteFacebookPage = 'https://www.facebook.com/AvitaGlobal/';
