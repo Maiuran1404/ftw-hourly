22147d1bbc5eda68908d3ca94ba6a6a77a068b33
Maiuran Loganathan <maiuran.loganathan@minerva.kgi.edu>
2021-07-31 16:57:51 +0200

    fix favicon

--- src/containers/ListingPage/SectionProcess.js
+++ src/containers/ListingPage/SectionProcess.js
@@ -12,32 +12,45 @@ const MIN_LENGTH_FOR_LONG_WORDS_IN_DESCRIPTION = 20;
 const SectionProcess = props => {
   const { description } = props;
   return description ? (
-    <div className={css.sectionDescription}>
-      <h2 className={css.descriptionTitle}>
-        <FormattedMessage id="ListingPage.processTitle" />
-      </h2>
-      <p className={css.description}>
-        <b> Book item &nbsp; </b>
-        <img src={PackageImage} alt={'Icons made by  Freepik (https://www.freepik.com) fromwww.flaticon.com'} height={40} width={40} />
-        <br />
+    <fr>
+      <div className={css.sectionDescription}>
+        <h2 className={css.descriptionTitle}>
+          <FormattedMessage id="ListingPage.contactTitle" />
+        </h2>
+        <p className={css.description}>
+          <b> Telefon: 48198693 &nbsp; </b>
+          <br />
+      Lurer du på noe som helst? Ring oss eller send oss en SMS så tar vi kontakt ASAP
+      <br /> <br />
+        </p>
+      </div>
+      {/* <div className={css.sectionDescription}>
+        <h2 className={css.descriptionTitle}>
+          <FormattedMessage id="ListingPage.processTitle" />
+        </h2>
+        <p className={css.description}>
+          <b> Book item &nbsp; </b>
+          <img src={PackageImage} alt={'Icons made by  Freepik (https://www.freepik.com) fromwww.flaticon.com'} height={40} width={40} />
+          <br />
         Check the availability of the item, book it, and make a secure payment right away.
         <br /> <br />
 
-        <b> We deliver it to your address &nbsp; </b>
-        <img src={CourierImage} alt={'Icons made by  Freepik (https://www.freepik.com) fromwww.flaticon.com'} height={40} width={40} />
-        <br />
+          <b> We deliver it to your address &nbsp; </b>
+          <img src={CourierImage} alt={'Icons made by  Freepik (https://www.freepik.com) fromwww.flaticon.com'} height={40} width={40} />
+          <br />
         Use your product for as long as you booked. We'll handle the delivery and pick up, so you can focus on enjoying the product.
         <br /> <br />
 
-        <b> We pick it up when time is up &nbsp;</b>
-        <img src={VanImage} alt={'Icons made by  Freepik (https://www.freepik.com) fromwww.flaticon.com'} height={40} width={40} />
-        <br />
+          <b> We pick it up when time is up &nbsp;</b>
+          <img src={VanImage} alt={'Icons made by  Freepik (https://www.freepik.com) fromwww.flaticon.com'} height={40} width={40} />
+          <br />
         Use your product for as long as you booked. We'll handle the delivery and pick up, so you can focus on enjoying the product.
         <br /> <br />
 
 
-      </p>
-    </div>
+        </p>
+      </div> */}
+    </fr>
   ) : null;
 };
 
